524b6684c2031b0e7f2c08a0256ef1e33630ed83
propet <luisdomingoaranda@gmail.com>
2016-09-09 12:00:19 +0200

    first commit

---
+++ ejecuta.py
@@ -0,0 +1,24 @@
+import os
+import glob
+
+# Crea carpeta para albergar los arhivos con las configuraciones del sistema
+os.system('sudo mkdir ./post')
+# Permisos a la carpeta
+os.system('sudo chmod -R 777 post')
+
+# Comprime hasta llegar a los 8 mm de altura respecto la base de la matriz
+os.system('mpirun -np 36 liggghts < in.plantilla1')
+
+# Relaja
+os.system('mpirun -np 36 liggghts < in.plantilla3')
+
+# Recupera el ultimo archivo con la configuracion del sistema
+newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas antes de retirar paredes
+os.system('mv %s ./dump1' % (newest))
+
+#Libera
+os.system('mpirun -np 36 liggghts < in.plantilla4')
+
+# Recupera el ultimo archivo con la configuracion del sistema
+newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas en reposos sin paredes
+os.system('mv %s ./dump2' % (newest))
